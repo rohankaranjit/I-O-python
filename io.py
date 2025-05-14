87e1c01ff5e21d3db35f873d452a2bd90e1a19b1
Rohan Karanjit <karanjitrohan@gmail.com>
2025-05-14 15:18:36 +0545

    Appending the file

--- io.py
+++ io.py
@@ -1,8 +1,12 @@
 #READS THE CONTENT IN READ ALL CONTENT AND IN READLINE ONE BY ONE
-f = open("practice.txt", "r")
-content = f.readline()         # reads entire content
-print(content)
-f.close()
+# f = open("practice.txt", "r")
+# content = f.readline()         # reads entire content
+# print(content)
+# f.close()
 
 
+#APPENDING THE DATA
+# f = open("practice.txt", "a")
+# f.write("\nThis is new text.")
+# f.close()
 
