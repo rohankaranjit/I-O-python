9fadf6d37680e78ef8f17ad989cad66220fed6df
Rohan Karanjit <karanjitrohan@gmail.com>
2025-05-14 15:16:33 +0545

    Started I/O in python

---
+++ io.py
@@ -0,0 +1,8 @@
+#READS THE CONTENT IN READ ALL CONTENT AND IN READLINE ONE BY ONE
+f = open("practice.txt", "r")
+content = f.readline()         # reads entire content
+print(content)
+f.close()
+
+
+
